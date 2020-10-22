82042fd374f0eead305f38d0368ef98e7de01946
Eric Chung <erc6863@utulsa.edu>
2020-10-21 21:54:45 -0500

    More messing with plant

--- Plant_App/app/src/main/java/com/example/plant_app/HomeActivity.kt
+++ Plant_App/app/src/main/java/com/example/plant_app/HomeActivity.kt
@@ -2,9 +2,11 @@ package com.example.plant_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class HomeActivity : AppCompatActivity() {
-    private lateinit var listView ListView
+    private lateinit var listView : ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
---
+++ Plant_App/app/src/main/java/com/example/plant_app/RecipeAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.plant_app
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class RecipeAdapter(private val context: Context,
+                    private val dataSource: ArrayList<Recipe>) : BaseAdapter() {
+
+    private val inflater: LayoutInflater
+            = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(position: Int): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemId(position: Int): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
+
+class Recipe {
+
+}
