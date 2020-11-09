d8a110c4371abd16152ac490462e207093ec37f6
Benjamin Bowdon <beb6426@utulsa.edu>
2020-11-08 21:08:40 -0600

    added template

---
+++ app/src/main/java/com/google/samples/apps/sunflower/viewmodels/PlantDetailViewModel.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.sunflower.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.google.samples.apps.sunflower.BuildConfig
+import com.google.samples.apps.sunflower.PlantDetailFragment
+import com.google.samples.apps.sunflower.data.GardenPlantingRepository
+import com.google.samples.apps.sunflower.data.PlantRepository
+import com.squareup.inject.assisted.Assisted
+import com.squareup.inject.assisted.AssistedInject
+import kotlinx.coroutines.launch
+
+/**
+ * The ViewModel used in [PlantDetailFragment].
+ */
+class PlantDetailViewModel @AssistedInject constructor(
+    plantRepository: PlantRepository,
+    private val gardenPlantingRepository: GardenPlantingRepository,
+    @Assisted private val plantId: String
+) : ViewModel() {
+
+    val isPlanted = gardenPlantingRepository.isPlanted(plantId)
+    val plant = plantRepository.getPlant(plantId)
+
+    fun addPlantToGarden() {
+        viewModelScope.launch {
+            gardenPlantingRepository.createGardenPlanting(plantId)
+        }
+    }
+
+    fun hasValidUnsplashKey() = (BuildConfig.UNSPLASH_ACCESS_KEY != "null")
+
+    @AssistedInject.Factory
+    interface AssistedFactory {
+        fun create(plantId: String): PlantDetailViewModel
+    }
+
+    companion object {
+        fun provideFactory(
+            assistedFactory: AssistedFactory,
+            plantId: String
+        ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return assistedFactory.create(plantId) as T
+            }
+        }
+    }
+}
